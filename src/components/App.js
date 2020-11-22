f26148fff48bc2431e68abb96e7ab1c751c57670
tincogs <tomoday91@gmail.com>
2020-11-21 21:49:28 -0600

    commit tree de217d09f71a250f55a676d994ca9b5390313f10

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,37 @@ class App extends React.Component {
     }
   }
 
+  onChangeType = (type) => {
+    this.setState({
+      ...this.state,
+      filters: {type}
+    })
+  }
+
+  onFindPetsClick = () => {
+    let url;
+    this.state.filters.type === "all" ?
+    url = "/api/pets" :
+    url = `/api/pets?type=${this.state.filters.type}`
+
+    fetch(url)
+    .then(resp => resp.json())
+    .then(results => {
+      this.setState({
+        pets: results
+      })
+    })
+  }
+
+  onAdoptPet = (petID) => {
+    console.log(petID)
+    const updatedPets = this.state.pets.map(pet => {
+      return pet.id === petID ? {...pet, isAdopted: true} : pet
+    })
+    this.setState({pets: updatedPets})
+  }
+
+
   render() {
     return (
       <div className="ui container">
@@ -24,10 +55,15 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters
+              onChangeType={this.onChangeType}
+              onFindPetsClick={this.onFindPetsClick}
+              />
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser
+              pets={this.state.pets}
+              onAdoptPet={this.onAdoptPet}/>
             </div>
           </div>
         </div>
